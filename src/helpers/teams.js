bcd3a59282e5c67c459ce0ff94464c77c6884acd
Domenico Flauto <domenico.flauto@eigentech.com>
2018-06-15 16:29:24 +0100

    updated profile pictures

--- src/helpers/teams.js
+++ src/helpers/teams.js
@@ -1,130 +1,162 @@
 export const TEAMS = {
   'RUS': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/ru.svg',
-    user: ''
+    user: '',
+    picture: 'https://drive.google.com/file/d/1g3oxh5_Nm3Vth4FKUceKBaCwgeEe8uZl/preview'
   },
   'KSA': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/sa.svg',
-    user: 'Jordan'
+    user: 'Jordan',
+    picture: 'https://drive.google.com/file/d/1ApsIIwRY5h5CDR-xmM5CcSgf8ZvjS2Pv/preview'
   },
   'EGY': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/eg.svg',
-    user: 'Jeremy'
+    user: 'Jeremy',
+    picture: 'https://drive.google.com/file/d/1EfHx6VIFEIslHUPL0PtXba3glH91f6mq/preview'
   },
   'URU': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/uy.svg',
-    user: 'Adam'
+    user: 'Adam',
+    picture: 'https://drive.google.com/file/d/1-5A0gnzwd6GV5YL3IHGaL28fiJggXs6a/preview'
   },
   'POR': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/pt.svg',
-    user: 'Simone'
+    user: 'Simone',
+    picture: 'https://drive.google.com/file/d/1vXH-_hyKHE_JrDe3ttQtajfxAXFCiUID/preview'
   },
   'ESP': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/es.svg',
-    user: 'Nick'
+    user: 'Nick',
+    picture: 'https://drive.google.com/file/d/1g3oxh5_Nm3Vth4FKUceKBaCwgeEe8uZl/preview'
   },
   'MAR': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/ma.svg',
-    user: 'Tom C'
+    user: 'Tom C',
+    picture: 'https://drive.google.com/file/d/1AOxdDXGN-K2HctXYNva8lsFE8AiXQ-pW/preview'
   },
   'IRN': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/ir.svg',
-    user: 'Josh'
+    user: 'Josh',
+    picture: 'https://drive.google.com/file/d/1g3oxh5_Nm3Vth4FKUceKBaCwgeEe8uZl/preview'
   },
   'FRA': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/fr.svg',
-    user: 'Andrei'
+    user: 'Andrei',
+    picture: 'https://drive.google.com/file/d/1xs9mIDO9Jc7Iwm--X33HfXqZTtAAkBP_/preview'
   },
   'AUS': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/au.svg',
-    user: 'David'
+    user: 'David',
+    picture: 'https://drive.google.com/file/d/14t4LYw5tR2DqbU83LCeClIAgx-GkloFm/preview'
   },
   'PER': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/pe.svg',
-    user: 'Hess'
+    user: 'Hess',
+    picture: 'https://drive.google.com/file/d/1W7fiaMgaXG_Bn4D4RdeMYXnXxOVlM4ll/preview'
   },
   'DEN': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/dk.svg',
-    user: 'Dom'
+    user: 'Dom',
+    picture: 'https://drive.google.com/file/d/1GPgBFaQEM-QIFHZNOEJEWGjZhP2x3ubl/preview'
   },
   'ARG': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/ar.svg',
-    user: 'Tameka'
+    user: 'Tameka',
+    picture: 'https://drive.google.com/file/d/1ETCG0oAoruSx0J5KjH0xwFH9XJGrOMiV/preview'
   },
   'ISL': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/is.svg',
-    user: 'Emily'
+    user: 'Emily',
+    picture: 'https://drive.google.com/file/d/1Wj1vgylOUkGf-Ffq5UlOTkHa_kM7fU7S/preview'
   },
   'NGA': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/ng.svg',
-    user: 'Julia'
+    user: 'Julia',
+    picture: 'https://drive.google.com/file/d/1Q2smoEntXwVJ7U4mDaoB3EEupjztD4hl/preview'
   },
   'BRA': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/br.svg',
-    user: 'Patrick'
+    user: 'Patrick',
+    picture: 'https://drive.google.com/file/d/13z46WR9Q9cZvxySTuMGaD2bOh21IzlkF/preview'
   },
   'SUI': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/ch.svg',
-    user: 'Marc'
+    user: 'Marc',
+    picture: 'https://drive.google.com/file/d/1g3oxh5_Nm3Vth4FKUceKBaCwgeEe8uZl/preview'
   },
   'CRC': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/cr.svg',
-    user: 'Jacob'
+    user: 'Jacob',
+    picture: 'https://drive.google.com/file/d/1-uzQG0lBKgVYKeBlDUGTRLWDjMzp70_b/preview'
   },
   'SRB': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/rs.svg',
-    user: 'Noam'
+    user: 'Noam',
+    picture: 'https://drive.google.com/file/d/1rlOXX-v7iTK4sCkGU20SLMiB2ukDhyfq/preview'
   },
   'GER': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/de.svg',
-    user: 'Ash'
+    user: 'Ash',
+    picture: 'https://drive.google.com/file/d/17cSmx7ISdKqymFTI0iuWWCLvhNkLNKcQ/preview'
   },
   'MEX': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/mx.svg',
-    user: 'Doug'
+    user: 'Doug',
+    picture: 'https://drive.google.com/open?id=1qNb_A24pz3c3MqUSWkZiIPlWZPBETqq7/preview'
   },
   'SWE': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/se.svg',
-    user: 'Julio'
+    user: 'Julio',
+    picture: 'https://drive.google.com/file/d/1cFYb2ej6QgydBHZtDDlZIVqcVPquV7wQ/preview'
   },
   'KOR': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/kr.svg',
-    user: 'Andrew'
+    user: 'Andrew',
+    picture: 'https://drive.google.com/file/d/1M0MR7TVoC-ZgGrdxzB1xUY6j2t0TQnot/preview'
   },
   'BEL': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/be.svg',
-    user: ''
+    user: '',
+    picture: 'https://drive.google.com/file/d/1g3oxh5_Nm3Vth4FKUceKBaCwgeEe8uZl/preview'
   },
   'PAN': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/pa.svg',
-    user: 'Gary'
+    user: 'Gary',
+    picture: 'https://drive.google.com/file/d/1mP1s9YUEOrooHlcSk3OBJei4Hne7jF8_/preview'
   },
   'ENG': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/gb-eng.svg',
-    user: 'Thanos'
+    user: 'Thanos',
+    picture: 'https://drive.google.com/file/d/1QNVMcxSPAbPhQZ7ar_29ke0bSeB0hCC8/preview'
   },
   'TUN': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/tn.svg',
-    user: 'Dan C'
+    user: 'Dan C',
+    picture: 'https://drive.google.com/file/d/1k9XCdUQbenYA_P2iBtK5iScSJYPXni8s/preview'
   },
   'JPN': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/jp.svg',
-    user: 'Gwyneth'
+    user: 'Gwyneth',
+    picture: 'https://drive.google.com/file/d/1jTWdOJOV1sm15xFELPFg4CbUguR5bUw0/preview'
   },
   'COL': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/co.svg',
-    user: ''
+    user: '',
+    picture: 'https://drive.google.com/file/d/1g3oxh5_Nm3Vth4FKUceKBaCwgeEe8uZl/preview'
   },
   'SEN': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/sn.svg',
-    user: 'Jamie'
+    user: 'Jamie',
+    picture: 'https://drive.google.com/file/d/1g3oxh5_Nm3Vth4FKUceKBaCwgeEe8uZl/preview'
   },
   'CRO': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/hr.svg',
-    user: 'Jiggins'
+    user: 'Jiggins',
+    picture: 'https://drive.google.com/file/d/1l49VzaBRHmlbU82V9E-cw03YXmC3gfIB/preview'
   },
   'POL': {
     flag: 'https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/pl.svg',
-    user: 'Stuart'
+    user: 'Stuart',
+    picture: 'https://drive.google.com/file/d/1loKiD1vkK0IWzxktcKTrvS5OVRA7Xqzz/preview'
   },
 };
